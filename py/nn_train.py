655a6e0b900cefe5c7425024907497164c474223
Owen Williams <williams.owen@gmail.com>
2021-07-18 20:04:52 +0100

    removing unused files

--- py/nn_train.py
+++ py/nn_train.py
@@ -37,7 +37,7 @@ for epoch in range(EPOCHS):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 10 == 9:    # print every 2000 mini-batches
+        if i % 10 == 0:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
